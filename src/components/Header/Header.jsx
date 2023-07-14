79e083e014b84f1916d95f68f3dc2f8fa040c923
Arnab Nath <arnabnath77@gmail.com>
2023-07-15 00:49:39 +0530

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="h-wrapper">
+        <div className="flexCenter paddings innerWidth h-container">
+            <img src="./logo.png" alt="logo" width={100} />
+            <div className="flexCenter h-menu">
+                <a href="" className="href">Residencies</a>
+                <a href="" className="href">Our Value</a>
+                <a href="" className="href">Contact Us</a>
+                <a href="" className="href">Get Started</a>
+                <button className='button'>
+                    <a href="" className="href">Contact</a>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
